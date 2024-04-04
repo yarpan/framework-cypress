a240df7168dc2eff904700ec2ceaa298eca1db75
Yaroslav Panchishyn <yarpan@gmail.com>
2024-04-04 22:20:19 +0300

    refactoring

---
+++ cypress/e2e/hooks.js
@@ -0,0 +1,46 @@
+before(() => {
+  // root-level hook
+  // runs once before all tests
+})
+
+beforeEach(() => {
+  // root-level hook
+  // runs before every test block
+})
+
+afterEach(() => {
+  // runs after each test block
+})
+
+after(() => {
+  // runs once all tests are done
+})
+
+
+
+
+describe('Hooks', () => {
+  before(() => {
+    // runs once before all tests in the block
+  })
+
+  beforeEach(() => {
+    // runs before each test in the block
+  })
+
+  afterEach(() => {
+    // runs after each test in the block
+  })
+
+  after(() => {
+    // runs once after all tests in the block
+  })
+})
+
+
+// The order of hook and test execution is as follows:
+// All before() hooks run (once)
+// Any beforeEach() hooks run
+// Tests run
+// Any afterEach() hooks run
+// All after() hooks run (once)
--- cypress/e2e/test-wiki.cy.js
+++ cypress/e2e/test-wiki.cy.js
@@ -47,3 +47,17 @@ describe('test2', () => {
     cy.location("pathname").should('eq', "/wiki/Main_Page");
   })
 })
+
+
+describe('test wiki menu spec', () => {
+  it('passes', () => {
+    cy.visit('https://wikipedia.org');
+    cy.get("[dir='ltr'] a").eq(0).click();
+    cy.location("host").should('eq', "en.wikipedia.org");
+    cy.location("pathname").should('eq', "/wiki/Main_Page");
+
+    cy.get("#p-vector-user-menu-overflow").contains("Create account");
+    cy.get("#p-vector-user-menu-overflow").contains("Log in");
+    cy.get("#vector-user-links-dropdown");
+  })
+})
